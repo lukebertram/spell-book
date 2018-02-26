ddaba95ab4f25c6aa7561c1bd53d262a359f00b6
christopher knight johnson and luke bertram <christopher+luke@>
2018-02-26 10:42:03 -0800

    add SpellSearch and SearchResult components to App

---
+++ src/SearchResult.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchResult(){
+  return(
+    <div>
+      <h3>Search Result Component</h3>
+    </div>
+  );
+}
+
+export default SearchResult;
---
+++ src/SpellSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SpellSearch(){
+  return(
+    <div>
+      <h3>Spell Search Component</h3>
+    </div>
+  );
+}
+
+export default SpellSearch;
