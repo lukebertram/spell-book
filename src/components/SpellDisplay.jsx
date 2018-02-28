92f4ecdacdab375fa60bad052d65be93d08bc985
Chris Knight Johnson and Luke Bertram <chris+luke@>
2018-02-27 16:31:26 -0800

    add spell display conditional rendering

--- src/components/SpellDisplay.jsx
+++ src/components/SpellDisplay.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { connect, dispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function SpellDisplay(){
+function SpellDisplay(props){
+  let spellInfoArea;
+  if (props.currentSpell){
+    spellInfoArea =
+      <div>
+        <h3>Current Spell:</h3>
+        <h1>{props.currentSpell.name}</h1>
+        <p>{props.currentSpell.desc}</p>
+      </div>;
+  } else {
+    spellInfoArea = <h3>No Spell Selected</h3>;
+  }
   return(
     <div>
-      <h3>Spell Display Component</h3>
+      {spellInfoArea}
     </div>
   );
 }
 
-export default SpellDisplay;
+SpellDisplay.propTypes = {
+  dispatch: PropTypes.func,
+  currentSpell: PropTypes.object
+}
+
+const mapStateToProps = state => {
+  return {
+    currentSpell: state.localSpells[state.selectedSpell]
+  }
+}
+
+export default connect(mapStateToProps)(SpellDisplay);
