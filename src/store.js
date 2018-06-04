14fc6e8afd9f748718e9d4c35b4d9b5485e12067
Luke Bertram <lukebertram@gmail.com>
2018-06-04 14:24:52 -0700

    add redux-devtools extension to store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 export default store;
