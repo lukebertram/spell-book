a7f007fb078dc80c5db7ac7f1a8697ad6b97e589
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-06-06 16:00:14 -0700

    change SpellDisplay to class component for UI state (checkbox)

--- src/components/SpellDisplay.jsx
+++ src/components/SpellDisplay.jsx
@@ -3,21 +3,29 @@ import { connect, dispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { toggleKnown } from '../actions';
 
-function SpellDisplay(props) {
+class SpellDisplay extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      spellIsKnown: this.props.mySpells[this.props.selectedSpell]
+    };
+  }
+  render(){
   let spellInfoArea;
-  if (props.selectedSpell) {
+  if (this.props.selectedSpell) {
     spellInfoArea = (
       <div>
-        <input type="checkbox" checked={props.mySpells[props.selectedSpell]} onChange={() => props.dispatch(toggleKnown(props.selectedSpell, props.spellCache, props.mySpells))}/>
+        <input type="checkbox" checked={this.state.spellIsKnown} onChange={() => this.props.dispatch(toggleKnown(this.props.selectedSpell, this.props.spellCache, this.props.mySpells))}/>
         <h3>Current Spell:</h3>
-        <h1>{props.currentSpell.name}</h1>
-        <p>{props.currentSpell.desc}</p>
+        <h1>{this.props.currentSpell.name}</h1>
+        <p>{this.props.currentSpell.desc}</p>
       </div>
     );
   } else {
     spellInfoArea = <h3>No Spell Selected</h3>;
   }
   return <div className="spell-info">{spellInfoArea}</div>;
+}
 }
 
 SpellDisplay.propTypes = {
